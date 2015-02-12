794ee674508baf4f694ab464f0dfc932a97f8e5f
Winho <Winho@KenLuo-MacMini-2.local>
2015-02-12 15:08:06 +0800

    Initial commit

---
+++ app/src/pro/java/com/example/winho/myapplication/Constants.java
@@ -0,0 +1,11 @@
+package com.example.winho.myapplication;
+
+/**
+ * Created by Winho on 15/2/12.
+ */
+public class Constants {
+    public enum Type{
+        FREE,FAID;
+    }
+    public static final Type type=Type.FAID;
+}
